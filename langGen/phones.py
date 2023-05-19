f6b8fa191bd739ab2ae155b96d5a35787d52a92c
Patrick Barrett <pbarrett520@yahoo.com>
2023-05-19 12:55:42 +0800

    can now call places of articulation as list objects

--- langGen/phones.py
+++ langGen/phones.py
@@ -1,17 +1,29 @@
-import csv
+import pandas as pd
 
 class Consonants:
-    def __init__(self,csv_row) -> None:
-        self.place_of_articulation = csv_row[0]
-        self.plosive = csv_row[1]
-        self.nasal = csv_row[2]
-        self.trill = csv_row[3]
-        self.tap = csv_row[4]
-        self.fricative = csv_row[5]
-        self.lateral_fricative = csv_row[6]
-        self.approximant = csv_row[7]
-        self.lateral_approximate = csv_row[8]
+    def __init__(self,csv_file) -> None:
+        self.phones = pd.read_csv(csv_file)
+        self.phones.columns = ['place','bilabial','labiodental','dental','alveolar','postalveolar','retroflex',
+                               'palatal','velar','uvular','pharyngal','glottal']
+        self.place_of_articulation = self.phones.place.to_list()
+        self.bilabial = self.phones.bilabial.to_list()
+        self.labiodental = self.phones.labiodental.to_list()
+        self.dental = self.phones.dental.to_list()
+        self.alveolar = self.phones.alveolar.to_list()
+        self.postalveolar = self.phones.postalveolar.to_list()
+        self.retroflex = self.phones.retroflex.to_list()
+        self.palatal = self.phones.palatal.to_list()
+        self.velar = self.phones.velar.to_list()
+        self.uvular = self.phones.uvular.to_list()
+        self.pharyngal = self.phones.pharyngal.to_list()
+        self.glottal = self.phones.pharyngal.to_list()
 
-class Vowels ():
-    def __init__(self) -> None:
-        pass
+
+class Vowels:
+    def __init__(self,csv_file) -> None:
+        self.phones = pd.read_csv(csv_file)
+        self.phones.columns = ['vowels','closeness','frontness','roundness']
+        self.vowels = self.phones.vowels.to_list()
+        self.closeness = self.phones.closeness.to_list()
+        self.frontness = self.phones.frontness.to_list()
+        self.roundness = self.phones.roundness.to_list()

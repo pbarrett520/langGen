e6108eec3241c7837085a447505f4bd640bbc5a9
Patrick Barrett <pbarrett520@yahoo.com>
2023-05-25 18:29:40 +0800

    added manners of articulation

--- langGen/phones.py
+++ langGen/phones.py
@@ -2,9 +2,11 @@ import pandas as pd
 
 class Consonants:
     def __init__(self,csv_file) -> None:
+        # Read csv into dataframe, label columns
         self.phones = pd.read_csv(csv_file)
         self.phones.columns = ['place','bilabial','labiodental','dental','alveolar','postalveolar','retroflex',
                                'palatal','velar','uvular','pharyngal','glottal']
+        # Convert columns into plain Python arrays, we don't need NP arrays
         self.place_of_articulation = self.phones.place.to_list()
         self.bilabial = self.phones.bilabial.to_list()
         self.labiodental = self.phones.labiodental.to_list()
@@ -17,6 +19,15 @@ class Consonants:
         self.uvular = self.phones.uvular.to_list()
         self.pharyngal = self.phones.pharyngal.to_list()
         self.glottal = self.phones.pharyngal.to_list()
+        # Do same conversion for rows, process is different because of Pandas weirdness
+        self.plosive = self.phones.iloc[[0]].values.flatten().tolist()
+        self.nasal = self.phones.iloc[[1]].values.flatten().tolist()
+        self.trill = self.phones.iloc[[2]].values.flatten().tolist()
+        self.tap = self.phones.iloc[[3]].values.flatten().tolist()
+        self.fricative = self.phones.iloc[[4]].values.flatten().tolist()
+        self.lateral_fricative = self.phones.iloc[[5]].values.flatten().tolist()
+        self.approximant = self.phones.iloc[[6]].values.flatten().tolist()
+        self.lateral_approximant = self.phones.iloc[[7]].values.flatten().tolist()
 
 
 class Vowels:

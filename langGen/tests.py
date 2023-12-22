e401e23a640e647388b48f0576c952e2d9d4bed5
pbarrett520 <pbarrett520@yahoo.com>
2023-12-21 19:04:37 -0500

    working to add utility for defining syllables in terms of CVC notation

--- langGen/tests.py
+++ langGen/tests.py
@@ -2,13 +2,26 @@ import pytest
 from phones import  *
 import re
 
-test_object = Phones('voiced_consonants.csv', 'voiceless_consonants.csv', 'vowels.csv',
+test_object_custom_regex = Phones('voiced_consonants.csv', 'voiceless_consonants.csv', 'vowels.csv',
 syll_struct = r"[ptkbdgθð]?[aeiouʌæ]?[aeiouʌæ][ptkbdgθð]?[ptkbdgθð]?")
-def test_func():
 
-    test_inventory = test_object.make_sylls(10)
+test_object_syllable_template = Phones('voiced_consonants.csv', 'voiceless_consonants.csv', 'vowels.csv',
+syll_struct=Syllable_patterns.polyneisian)
 
-    assert len(test_object.syll_inventory) == 10
+print(test_object_syllable_template.make_sylls(10))
+def test_make_sylls():
 
-    for i in test_inventory:
-        assert re.match(test_object.syll_struct, i)
+    test_inventory_1 = test_object_custom_regex.make_sylls(10) # generate a small syllable inventory
+
+    assert len(test_object_custom_regex.syll_inventory) == 10 # make sure the number of elements is correct
+
+    for i in test_inventory_1:
+        assert re.match(test_object_custom_regex.syll_struct, i) # make sure syllables match pattern
+
+def test_syllable_patterns():
+
+    test_inventory_2 = test_object_custom_regex.make_sylls(10)
+
+
+    for i in test_inventory_2:
+        assert re.match(test_object_custom_regex.syll_struct, i)
